9edea608bf3b2989da39a064bb6484076f984567
soojae <developersoojae@gmail.com>
2020-04-22 00:31:52 +0900

    [앵귤러] 앵귤러 연습

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,6 +8,7 @@ import {Recipe} from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://p0.pikrepo.com/preview/101/421/grilled-pork-belly-with-green-salad.jpg'),
     new Recipe('A Test Recipe', 'This is simply a test', 'https://p0.pikrepo.com/preview/101/421/grilled-pork-belly-with-green-salad.jpg')
   ];
 
