d804d7c8a8aa59752c76d8e914fd52e262bc745d
Jerry <jerry@exntu.com>
2020-04-26 23:50:50 +0900

    [앵귤러] 앵귤러 연습

--- cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -1,4 +1,15 @@
-import {Component, Input, OnInit, ViewEncapsulation} from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  DoCheck,
+  ElementRef,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+  ViewChild,
+  ViewEncapsulation
+} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -6,14 +17,32 @@ import {Component, Input, OnInit, ViewEncapsulation} from '@angular/core';
   styleUrls: ['./server-element.component.css'],
   encapsulation: ViewEncapsulation.ShadowDom
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements OnInit, OnChanges, DoCheck, AfterViewInit {
+
+  public test = 'constructor';
   @Input('srvElement')
   element: { type: string, name: string, content: string };
+  @Input() name: string;
+  @ViewChild('heading') header: ElementRef;
 
   constructor() {
+    console.log('constructor called', this.test);
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log(this.test);
   }
 
   ngOnInit(): void {
+    console.log(this.header.nativeElement.textContent);
+    console.log('ngOninit called');
   }
 
+  ngAfterViewInit(){
+    console.log('text',this.header.nativeElement.textContent);
+  }
+
+  ngDoCheck(): void {
+    console.log('ngDoCheck Called!');
+  }
 }
