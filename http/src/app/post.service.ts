5482fc82c6bf1789e8dbabbe42b7fd2dd29caa62
soojae <developersoojae@gmail.com>
2020-06-04 23:20:17 +0900

    [HTTP 통신] 헤더 옵션

--- http/src/app/post.service.ts
+++ http/src/app/post.service.ts
@@ -1,7 +1,7 @@
 import {Injectable} from "@angular/core";
-import {HttpClient, HttpHeaders, HttpParams} from "@angular/common/http";
+import {HttpClient, HttpEventType, HttpHeaders, HttpParams} from "@angular/common/http";
 import {Post} from "./post.model";
-import {map, catchError} from "rxjs/operators";
+import {map, catchError, tap} from "rxjs/operators";
 import {Subject, throwError} from "rxjs";
 
 @Injectable({providedIn: 'root'})
@@ -18,7 +18,10 @@ export class PostsService {
     this.http
       .post<{ name: string }>(
         'https://ng-complete-guide-43608.firebaseio.com/posts.json',
-        postData
+        postData,
+        {
+          observe: 'response'
+        }
       ).subscribe(
       responseData => {
         console.log(responseData);
@@ -59,6 +62,19 @@ export class PostsService {
   }
 
   deletePosts() {
-    return this.http.delete('https://ng-complete-guide-43608.firebaseio.com/posts.json');
+    return this.http.delete(
+      'https://ng-complete-guide-43608.firebaseio.com/posts.json',
+      {
+        observe: 'events',
+        responseType: 'text',
+      }
+    ).pipe(tap(event => {
+      if (event.type === HttpEventType.Sent) {
+        console.log(event.type);
+      }
+      if (event.type === HttpEventType.Response) {
+        console.log(event.body);
+      }
+    }));
   }
 }
