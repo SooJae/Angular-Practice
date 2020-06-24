25e5db96ce27d6146a3b8701b060d822c1b142a1
soojae <developersoojae@gmail.com>
2020-06-24 22:05:43 +0900

    [모듈] 모듈 분리

--- practice/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ practice/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -7,8 +7,8 @@ import {
 
 import {Ingredient} from '../../shared/ingredient.model';
 import {ShoppingListService} from '../shopping-list.service';
-import {NgForm} from "@angular/forms";
-import {Subscription} from "rxjs";
+import {NgForm} from '@angular/forms';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -26,25 +26,30 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit() {
-    this.subscription = this.slService.startedEditing
-      .subscribe(
-        (index: number) => {
-          this.editedItemIndex = index;
-          this.editMode = true;
-          this.editedItem = this.slService.getIngredient(index);
-          this.slForm.setValue({
-            name: this.editedItem.name,
-            amount: this.editedItem.amount
-          });
-        }
-      );
+    this.subscription = this.slService.startedEditing.subscribe(
+      (index: number) => {
+        this.editedItemIndex = index;
+        this.editMode = true;
+        this.editedItem = this.slService.getIngredient(index);
+        this.slForm.setValue({
+          name: this.editedItem.name,
+          amount: this.editedItem.amount
+        });
+      }
+    );
   }
 
   onSubmit(form: NgForm) {
     const value = form.value;
-    const newIngredient = new Ingredient(value.name, value.amount);
+    const newIngredient = new Ingredient(
+      value.name,
+      value.amount
+    );
     if (this.editMode) {
-      this.slService.updateIngredient(this.editedItemIndex, newIngredient);
+      this.slService.updateIngredient(
+        this.editedItemIndex,
+        newIngredient
+      );
     } else {
       this.slService.addIngredient(newIngredient);
     }
